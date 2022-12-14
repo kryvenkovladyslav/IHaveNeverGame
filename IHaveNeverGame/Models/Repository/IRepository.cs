2962c357bc902c908399f03e90200f28231d6e8a
feredico.gattuso <feredico.gattuso@gmail.com>
2022-12-14 18:37:36 +0200

    Добавьте файлы проекта.

---
+++ IHaveNeverGame/Models/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace IHaveNeverGame.Models.Repository
+{
+    public interface IRepository<T>
+    {
+        public IEnumerable<T> Entities { get; set; }
+    }
+}
---
+++ IHaveNeverGame/Models/Repository/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IHaveNeverGame.Models.Repository
+{
+    [Serializable]
+    public class Repository<T> : IRepository<T>
+    {
+        public IEnumerable<T> Entities { get; set; }
+
+        public Repository() { }
+        public Repository(IEnumerable<T> entities) => Entities = entities;
+    }
+}
